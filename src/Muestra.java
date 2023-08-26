ae24b9a8610d0795883a81347b48b3cc77c0018d
iveth-cocha <maria.iveth16@outlook.es>
2023-08-26 04:15:45 -0500

    ingresos en scrollPanel

--- src/Muestra.java
+++ src/Muestra.java
@@ -3,6 +3,7 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
+import javax.swing.JScrollPane;
 
 
 public class Muestra {
@@ -28,7 +29,8 @@ public class Muestra {
     String suelx;
     String idx ;
     String contrax;
-    String fingrx ;
+    String fingrx;
+
 
     public Muestra() {
         listadoDeCajerosButton.addActionListener(new ActionListener() {
@@ -38,12 +40,38 @@ public class Muestra {
 
             }
         });
+        ingresarInformacionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+               /* idx= String.valueOf(Integer.parseInt(id.getText()));
+                nomx=nombre.getText().trim();
+                apex=apellido.getText().trim();
+                rolx=rol.getText().trim();
+                contrax=contra.getText().trim();
+                suelx= String.valueOf(Float.parseFloat(salario.getText())).trim();
+                fingrx=fIngreso.getText().trim();
+                Ingresar(idx, nomx, apex, rolx, contrax, suelx,fingrx );*/
+
+                idx = id.getText();
+                nomx = nombre.getText().trim();
+                apex = apellido.getText().trim();
+                rolx = rol.getText().trim();
+                contrax = contra.getText().trim();
+                suelx = salario.getText().trim();
+                fingrx = fIngreso.getText().trim();
+                Ingresar(idx, nomx, apex, rolx, contrax, suelx, fingrx);
+
+
+            }
+        });
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Registro de Personas");
         frame.setContentPane(new Muestra().rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 150);
         frame.setLocationRelativeTo(null);
         frame.pack();
         frame.setVisible(true);
@@ -88,43 +116,21 @@ public class Muestra {
         }
     }
 
-    public void Ingresar(){
-        //genera columnas de la tabla
-        DefaultTableModel model = new DefaultTableModel();
-        model.addColumn("idUsuario");
-        model.addColumn("nombre");
-        model.addColumn("Apellido");
-        model.addColumn("Rol");//columna tipoUsuario
-        model.addColumn("Contraseña");
-        model.addColumn("Salario");
-        model.addColumn("FechaIngreso");
-
-        // Poner el modelo hecho en el Jtable
-        visor.setModel(model);
-
-        //arreglo que almnacena datos
-        String [] informacion=new String[7];//especifico el numero de columnas
-
-        try{
-            Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            Statement stmt= conn.createStatement();
-            ResultSet rs= stmt.executeQuery(QUERY);
-
-            while (rs.next()){
-                informacion[0]=rs.getString(1);//num de columna
-                informacion[1]=rs.getString(2);
-                informacion[2]=rs.getString(3);
-                informacion[3]=rs.getString(4);
-                informacion[4]=rs.getString(5);
-                informacion[5]=rs.getString(6);
-                informacion[6]=rs.getString(7);
-
-                //una fila por cada ingreso
-                model.addRow(informacion);
-            }
-        } catch (SQLException e) {
-            //throw new RuntimeException(e);
-            JOptionPane.showMessageDialog(null,"Error"+e.toString());
+    public void Ingresar(String idU, String nom, String ape, String trol, String cont, String suel, String ingre){
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement pstmt = conn.prepareStatement("INSERT INTO Usuario (idUsuario, nombre, apellido, tipoUsuario, contraseña, salario, fechaContratacion) VALUES (?, ?, ?, ?, ?, ?, ?)")) {
+            pstmt.setInt(1, Integer.parseInt(idU));
+            pstmt.setString(2, nom);
+            pstmt.setString(3, ape);
+            pstmt.setString(4, trol);
+            pstmt.setString(5, cont);
+            pstmt.setDouble(6, Double.parseDouble(suel));
+            pstmt.setString(7, ingre);
+            pstmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Informacion ingresada correctamente");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
 }
